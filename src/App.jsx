d1206eb06c665f37f19d42ce1b0bf907bf0d9a09
Luca <lucatonello097@gmail.com>
2024-07-06 17:09:29 -0300

    changes only the name with user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,118 @@
 import { useState } from 'react'
 import './App.css'
+import { person } from './data.js';
 
-const person = {
-  generalInfo: {
-    name: 'John',
-    ocupation: 'Software engineer',
-    email: 'john@gmail.com',
-    phone: '555-555-555',
-  },
-  education: [
-  {
-    school: 'Harvard',
-    major: 'BS in Computer Science',
-    date: '2014-2018'
-  },
-  {
-    school: 'Phillips Academy Andover',
-    major: 'High school diploma',
-    date: '2009-2013'
+function Background({ type, content }) {
+  if (type === 'education') {
+    return (
+      <div class="education-container">
+        {content.map((elem, index) => (
+          <div key={index} className="education">
+            <p><b>School: </b>{elem.school}</p>
+            <p><b>Major: </b>{elem.major}</p>
+            <p><b>Date: </b>{elem.date}</p>
+          </div>
+        ))} 
+      </div>
+    );
   }
-  ],
-  experience: [
-  {
-    company: 'Google',
-    position: 'Software Engineer',
-    responsibilities: 'Mantain and improve existing code',
-    date: '2019-2022' 
-  },
-  {
-    company: 'Open AI',
-    position: 'Software Engineer',
-    responsibilities: 'Mantain code and add new features',
-    date: '2022-Present'
+  else if (type === 'experience') {
+    return (
+      <div className="experience-container">
+        {content.map((elem, index) => (
+          <div key={index} className="experience">
+            <p><b>Company: </b>{elem.company}</p>
+            <p><b>Position: </b>{elem.position}</p>
+            <p><b>Responsabilities: </b>{elem.responsibilities}</p>
+            <p><b>Date: </b>{elem.date}</p>
+          </div>
+        ))} 
+      </div>
+    )
   }
-  ],
-};
-
-function Education({ education }) {
-  return (
-    <div class="education-container">
-      {education.map((elem, index) => (
-        <div key={index} className="education">
-          <p><b>School: </b>{elem.school}</p>
-          <p><b>Major: </b>{elem.major}</p>
-          <p><b>Date: </b>{elem.date}</p>
-        </div>
-      ))} 
-    </div>
-  );
+  else {
+    return null;
+  }
 }
 
+
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [formData, setFormData] = useState(person);
 
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      generalInfo: {
+        ...prevState.generalInfo,
+        [name]: value
+      }
+    }));
+    
+  }
+  console.log(formData.generalInfo.name);
   return (
-    <>
-      <h1>{person.generalInfo.name}</h1>
-      <h2>{person.generalInfo.ocupation}</h2>
+    <div className="page-wrapper">
+      <div className="input-container">
+        <h3>Basic information</h3>
+        <label htmlFor="name">Name</label>
+        <input 
+          type="text"
+          id="name"
+          name="name"
+          value={formData.generalInfo.name}
+          onChange={handleInputChange}
+        />
 
-      <p>Email: {person.generalInfo.email}</p>
-      <p>Phone: {person.generalInfo.phone} </p>
+        <label htmlFor="Ocupation">Ocupation</label>
+        <input type="text" />
 
-      <h1>Education</h1>
-      <hr />
-      
-      <Education education={person.education} />
-      <h1>Experience</h1>
-      <hr />
-      <p><b>Company: </b>{person.experience[0].company}</p>
-      <p><b>Position: </b>{person.experience[0].position}</p>
-      <p><b>Responsabilities: </b>{person.experience[0].responsibilities}</p>
-      <p><b>Date: </b>{person.experience[0].date}</p>
-    </>
+        <label htmlFor="Email">Email</label>
+        <input type="text" />
+
+        <label htmlFor="Phone">Phone</label>
+        <input type="text" />
+  
+        <h3>Education</h3>
+        <label htmlFor="School">School</label>
+        <input type="text" />
+
+        <label htmlFor="Major">Major</label>
+        <input type="text" />
+
+        <label htmlFor="Date">Date</label>
+        <input type="text" />
+
+        <h3>Experience</h3>
+        <label htmlFor="Company">Company</label>
+        <input type="text" />
+
+        <label htmlFor="Position">Position</label>
+        <input type="text" />
+
+        <label htmlFor="Responsabilities">Responsabilities</label>
+        <input type="text" />
+
+        <label htmlFor="Date">Date</label>
+        <input type="text" />
+      </div>
+
+      <div className="cv-container">
+        <h1 id="name">{formData.generalInfo.name}</h1>
+        <h2>{person.generalInfo.ocupation}</h2>
+
+        <p><b>Email: </b>{person.generalInfo.email}</p>
+        <p><b>Phone: </b>{person.generalInfo.phone} </p>
+
+        <h1>Education</h1>
+        <hr />
+        <Background type="education" content={person.education} />
+
+        <h1>Experience</h1>
+        <hr />
+        <Background type="experience" content={person.experience} />
+      </div>
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const person = {
+    generalInfo: {
+      name: 'John',
+      ocupation: 'Software engineer',
+      email: 'john@gmail.com',
+      phone: '555-555-555',
+    },
+    education: [
+    {
+      school: 'Harvard',
+      major: 'BS in Computer Science',
+      date: '2014-2018'
+    },
+    {
+      school: 'Phillips Academy Andover',
+      major: 'High school diploma',
+      date: '2009-2013'
+    }
+    ],
+    experience: [
+    {
+      company: 'Google',
+      position: 'Software Engineer',
+      responsibilities: 'Mantain and improve existing code',
+      date: '2019-2022' 
+    },
+    {
+      company: 'Open AI',
+      position: 'Software Engineer',
+      responsibilities: 'Mantain code and add new features',
+      date: '2022-Present'
+    }
+    ],
+  };
+  
